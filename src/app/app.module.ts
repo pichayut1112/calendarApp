f030da2cb0f5cba0f9b72162843236e0e0d05867
its-pichayut_p <pichayut.pp@gmail.com>
2020-01-28 14:51:08 +0700

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { ScheduleModule, RecurrenceEditorModule, DayService, WeekService, WorkWeekService, MonthService, MonthAgendaService,TimelineViewsService, TimelineMonthService } from '@syncfusion/ej2-angular-schedule';
+import { CalendarHZComponent } from './calendar-hz/calendar-hz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CalendarComponent,
+    CalendarHZComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'home', component:HomeComponent},
+      {path:'calendar', component:CalendarComponent},
+      {path:'calendarhz', component:CalendarHZComponent}
+    ]),
+    ScheduleModule, RecurrenceEditorModule
   ],
-  providers: [],
+  providers: [DayService, WeekService, WorkWeekService, MonthService, MonthAgendaService,TimelineViewsService, TimelineMonthService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
